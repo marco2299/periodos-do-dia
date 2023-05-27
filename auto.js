903950c568bd9911713429db1e37cd3df15b60bf
Marco Antonio <88465561+marco2299@users.noreply.github.com>
2023-05-27 11:29:26 -0300

    Update auto.js
    Atualização para que o usuário possa visualizar os períodos do dia, apenas clicando nos emojis.

--- auto.js
+++ auto.js
@@ -1,34 +1,53 @@
-function iniciar(){
-    var txt = document.getElementById("txt");
-    var tempo = new Date();
-    var hora = tempo.getHours();
-    var minutos = tempo.getMinutes();
-    var img = document.getElementById('bom');
-    if(minutos < 10){ //verifica se é preciso adicionar um 0 (por questões de estilo)
-        txt.innerText += ` ${hora}:0${minutos}`;
+var cont = 0;
+function manha() {
+    iniciar(7, cont);
+}
+
+function tarde() {
+    iniciar(17, cont);
+}
+
+function noite() {
+    iniciar(19, cont);
+}
+function madrugada(){
+    iniciar(4, cont);
+}
+
+function iniciar(x, cont) {
+var tempo = new Date();
+var hora = tempo.getHours();
+var txt = document.getElementById("txt");
+var minutos = tempo.getMinutes();
+var img = document.getElementById('bom');
+    if(cont === 0){
+        hora = x;
+    }
+
+    if(minutos < 10){
+        txt.innerText = ` Olá! Agora são exatamente ${hora}:0${minutos}`;
     }
     else{
-        txt.innerText += ` ${hora}:${minutos}`;
+        txt.innerText = `Olá! Agora são exatamente ${hora}:${minutos}`;
     }
-    if (hora >= 6 && hora < 12){
+    if(hora >= 6 && hora < 12){
         txt.innerHTML += `<br> </p> Bom dia!`;
         img.src = "imagens/dia.jpg";
         document.body.style.backgroundColor = 'rgba(71, 143, 250, 0.712)';
-    } 
-    else if (hora >=12 && hora < 18){
+    }
+    else if(hora >= 12 && hora < 18){
         txt.innerHTML += `<br> </p> Boa Tarde!`;
         img.src = "imagens/tarde.jpg";
         document.body.style.backgroundColor = 'rgba(255, 117, 4, 0.705)';
-    } 
+    }
     else if(hora >= 18 && hora < 24){
         txt.innerHTML += `<br> </p> Boa Noite!`;
         img.src = "imagens/noite.jpg";
-        document.body.style.backgroundColor ='rgba(27, 17, 105, 0.925)';  
+        document.body.style.backgroundColor = 'rgba(27, 17, 105, 0.925)';
     }
     else{
         txt.innerHTML += `<br> </p> Boa Madrugada!`;
         img.src = "imagens/cat.jpg";
-        document.body.style.backgroundColor ='rgba(14, 10, 44, 0.925)';
+        document.body.style.backgroundColor = 'rgba(14, 10, 44, 0.925)';
     }
 }
-
